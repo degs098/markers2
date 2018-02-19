c3890369ef93ab22cc44429e2312913c41596725
Daniel Gallego <gallegodaniel9004@gmail.com>
2018-02-18 22:52:34 -0500

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { 
+  MatFormFieldModule, 
+  MatButtonModule, 
+  MatCheckboxModule, 
+  MatChipsModule, 
+  MatToolbarModule,
+  MatSidenavModule     
+} from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,    
+    MatFormFieldModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatToolbarModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
